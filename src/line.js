626306f44f8a7e6b83c4cf81d088cca514a3df7a
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-05-29 11:31:25 +0530

    Line can be drawn on the html page

--- src/line.js
+++ src/line.js
@@ -1,28 +1,105 @@
 const { Point } = require('./point.js');
 
+class Style {
+  constructor(styleRules) {
+    this.styleRules = { ...styleRules };
+  }
+
+  create() {
+    return Object.entries(this.styleRules).map(([attr, value]) => {
+      return `${attr}: ${value}`;
+    }).join(';');
+  }
+
+  addRule(rule) {
+    this.styleRules = { ...this.styleRules, ...rule };
+  }
+}
+
+class Div {
+  constructor({ style, content }) {
+    this.style = style;
+    this.content = content;
+  }
+  html() {
+    return `<div style="${this.style.create()}">${this.content || ''}</div>`
+  }
+}
+
+class Canvas {
+  constructor({ width, height }) {
+    this.width = width;
+    this.height = height;
+  }
+  draw(content) {
+    const style = new Style({
+      width: `${this.width}px`,
+      height: `${this.height}px`,
+      position: 'relative',
+      border: '1px solid black',
+    });
+    return new Div({ style, content }).html();
+  }
+}
+
+const transformRotate = (angle) => {
+  return `rotate(${angle}rad)`;
+};
+
+const angle = (slope) => {
+  return Math.atan(slope);
+};
+
 class Line {
   constructor(start, end) {
-    this.start = start;
-    this.end = end;
+    this.start = new Point(start.x, start.y);
+    this.end = new Point(end.x, end.y);
   }
+
   midPoint() {
     const xMid = (this.start.x + this.end.x) / 2
     const yMid = (this.start.y + this.end.y) / 2
     return new Point(xMid, yMid);
   }
+
   length() {
     const xDiff = this.start.x - this.end.x;
     const yDiff = this.start.y - this.end.y;
     return Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
   }
+
   isParallelTo(line) {
     return this.slope() === line.slope();
   }
+
   slope() {
     const yDelta = this.start.y - this.end.y;
     const xDelta = this.start.x - this.end.x;
     return yDelta / xDelta;
   }
+
+  toHTML() {
+    const style = new Style({
+      'background-color': 'black',
+      'transform-origin': 'left top',
+      position: 'absolute',
+      left: `${this.start.x}px`,
+      top: `${this.start.y}px`,
+      width: `${this.length()}px`,
+      height: '2px',
+      transform: transformRotate(angle(this.slope())),
+    });
+    return new Div({ style }).html();
+  }
+
+  draw() {
+    const canvas = new Canvas({
+      height: 800,
+      width: 900
+    });
+    const content = canvas.draw(this.toHTML());
+    fs.writeFileSync('./line.html', content, 'utf8');
+  }
 }
 
-exports.createLine = createLine;
+exports.Line = Line;
